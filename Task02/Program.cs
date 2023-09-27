722631b9f4f8882f4003780edfe1474d8ffaaa74
Arghara <arghara@gmail.com>
2023-09-27 11:35:19 +0300

    Решение второй задачи

---
+++ Task02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        string input = Console.ReadLine();
+
+        int sum = CalculateDigitSum(input);
+
+        Console.WriteLine($"Сумма цифр в числе = {sum}");
+    }
+
+    static int CalculateDigitSum(string input)
+    {
+        int sum = 0;
+
+        foreach (char c in input)
+        {
+            if (Char.IsDigit(c))
+            {
+                sum += int.Parse(c.ToString());
+            }
+        }
+
+        return sum;
+    }
+}
